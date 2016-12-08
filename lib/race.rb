191b56bae96b9e01ceb96b0c1b6a38e5ba22eec5
myungok-star <myungok.star@gmail.com>
2016-12-07 16:53:49 -0800

    winner and loser methods defined, test has been completed

--- lib/race.rb
+++ lib/race.rb
@@ -10,4 +10,20 @@ class Race
 
   end
 
-  
+  def winner
+    if cars[0].speed > cars[1].speed
+      cars[0]
+    else
+      cars[1]
+    end
+  end
+
+  def loser
+    if winner == cars[0]
+      cars[1]
+    else
+      cars[0]
+    end
+  end
+
+end
